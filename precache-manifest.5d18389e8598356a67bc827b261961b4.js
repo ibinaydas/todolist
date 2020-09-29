f5f17e33c26d9a2c34bb0420fcf7f2b7ce2ac6e5
Binay Das <ibinaydas@gmail.com>
2020-09-29 21:58:59 +0530

    Updates

---
+++ precache-manifest.5d18389e8598356a67bc827b261961b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29cd65114036f8985af6ba51e40c4f81",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8477bea8355c478f2a16",
+    "url": "/todolist/static/css/main.7b1a8e89.chunk.css"
+  },
+  {
+    "revision": "6297f6fbfebb01ec555a",
+    "url": "/todolist/static/js/2.0774eb6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist/static/js/2.0774eb6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8477bea8355c478f2a16",
+    "url": "/todolist/static/js/main.9b527a62.chunk.js"
+  },
+  {
+    "revision": "130fd48b1b39e8c6b47c",
+    "url": "/todolist/static/js/runtime-main.5f712ceb.js"
+  }
+]);
